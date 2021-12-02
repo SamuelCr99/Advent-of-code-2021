8e277804a66422efdb55a440aadcff8a67643609
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-12-02 23:34:33 +0100

    Fixed day 1 and 2!

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,4 +1,25 @@
-﻿for (int i = 0; i < 1000; i++)
+﻿using System;
+
+namespace Day1
 {
-    Console.WriteLine("Hello");
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = 0;
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            int smaller = Int32.Parse(lines[0]);
+            for (int i = 3; i < lines.Length; i++){
+                int first = Int32.Parse(lines[i-3]);
+                int second = Int32.Parse(lines[i-2]);
+                int third = Int32.Parse(lines[i-1]);
+                int fourth = Int32.Parse(lines[i]);
+
+                if(fourth > first) {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
 }
---
+++ Day2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Day2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int depth = 0;
+            int pos = 0;
+            int aim = 0; 
+
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+            for (int i = 0; i < lines.Length; i++){
+                String line = lines[i];
+                if(line.Contains("forward")){
+                    int val = Convert.ToInt32(Char.GetNumericValue(line[8]));
+                    pos += val;
+                    depth += val*aim;
+
+                }
+                else if(line.Contains("up")){
+                    int val = Convert.ToInt32(Char.GetNumericValue(line[3]));
+                    aim -= val;
+
+                }
+                else if(line.Contains("down")){
+                    int val = Convert.ToInt32(Char.GetNumericValue(line[5]));
+                    aim   += val;
+                }
+            }
+            Console.WriteLine(depth*pos);
+        }
+    }
+}
