a20d8e85c955f0ba3a0dc542aeaa93c268cc121b
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-12-04 22:42:38 +0100

    Some more qestions, day3q2 is not done though

---
+++ Day3/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Day3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines("input.txt");
+            int[] countArray = {0,0,0,0,0,0,0,0,0,0,0,0};
+            for (int i = 0; i < input.Length; i++){
+                String currentLine = input[i];
+                for (int k = 0; k < 12; k++){
+                    if(currentLine[k] == '1'){
+                        countArray[k] += 1;
+                    }
+                    else{
+                        countArray[k] -= 1;
+                    }
+                }
+            }
+            int[] newArrayGamma = getCorrectGamma(countArray);
+            int[] newArrayEpsilon = getCorrectEpsilon(countArray);
+
+            int gamma = binaryToDecimal(newArrayGamma);
+            int epsilon = binaryToDecimal(newArrayEpsilon);
+
+            Console.WriteLine(gamma*epsilon);
+
+        }
+
+        private static int[] getCorrectGamma(int [] arr){
+            int[] newArray = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++){
+                if(arr[i] > 0){
+                    newArray[i] = 1;
+                }
+                else{
+                    newArray[i] = 0;
+                }
+            }
+            return newArray;
+        }
+
+
+        private static int[] getCorrectEpsilon(int [] arr){
+            int[] newArray = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++){
+                if(arr[i] < 0){
+                    newArray[i] = 1;
+                }
+                else{
+                    newArray[i] = 0;
+                }
+            }
+            return newArray;
+        }
+        
+
+        private static int binaryToDecimal (int[] arr){
+            int val = 0; 
+            
+            for (int i = arr.Length-1; i >= 0; i--){
+                val += arr[i] * (int)(Math.Pow(2,arr.Length-1-i));
+            }
+            return val;
+        }
+
+    }
+}
---
+++ Day3Q2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Day3Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = System.IO.File.ReadAllLines("input.txt");
+            for (int i = 0; i < input[0].Length; i++){
+                int numberOfOne = 0;
+                for (int k = 0; k < input.Length; k++){
+                    if (input[k] == null){
+                        continue;
+                    }
+                    String line = input[k];
+                    if (line[i] == '1'){
+                        numberOfOne += 1;
+                    }
+                    else{
+                        numberOfOne -= 1;
+                    }
+                }
+                char key = '0';
+                if(numberOfOne > 0){
+                    key = '1';
+                }
+                for (int k = 0; k < input.Length; k++){
+                    String line = input[k];
+                    if (line[i] != key){
+                        input[k] = null;
+                    }
+                 
+                }
+            }
+
+    }
+    }
+}
