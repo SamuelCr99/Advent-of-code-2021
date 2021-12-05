fc3fd21e3a532d060100eaa5347ce30a0ddc205f
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-12-05 15:23:42 +0100

    Day3Q2 finally finished

--- Day3Q2/Program.cs
+++ Day3Q2/Program.cs
@@ -5,35 +5,96 @@ namespace Day3Q2
     class Program
     {
         static void Main(string[] args)
-        {
+        {//So this code is really ugly but I spent 2 hours bug fixing it so now it will have to look like this! 
             string[] input = System.IO.File.ReadAllLines("input.txt");
-            for (int i = 0; i < input[0].Length; i++){
-                int numberOfOne = 0;
-                for (int k = 0; k < input.Length; k++){
-                    if (input[k] == null){
-                        continue;
-                    }
-                    String line = input[k];
-                    if (line[i] == '1'){
-                        numberOfOne += 1;
-                    }
-                    else{
-                        numberOfOne -= 1;
-                    }
-                }
-                char key = '0';
-                if(numberOfOne > 0){
-                    key = '1';
-                }
-                for (int k = 0; k < input.Length; k++){
-                    String line = input[k];
-                    if (line[i] != key){
-                        input[k] = null;
-                    }
-                 
+
+            int oxygenGeneration = calcValue(input,'1','0');
+            input = System.IO.File.ReadAllLines("input.txt"); //Resets the array. 
+            int cO2Generation = calcValue(input,'0','1');
+
+            Console.WriteLine(oxygenGeneration*cO2Generation); 
+            
+        }
+
+        private static int binaryToDecimal (string s){
+            int val = 0; 
+            for (int i = s.Length-1; i >= 0; i--){
+                int Currentval = s[i] - '0';
+                val += Currentval * (int)(Math.Pow(2,s.Length-1-i));
+            }
+            return val;
+        }
+
+        private static string findNonNull (string [] array){
+            for (int i = 0; i < array.Length; i++){
+                if (array[i] != "null"){
+                    return array[i];
                 }
             }
+            throw new InvalidOperationException("File should always find one value, other code is wrong");
+        }
 
-    }
+        private static void showArray (string[] array){
+            for(int i = 0; i < array.Length; i++){
+                Console.WriteLine(array[i]);
+            }
+            Console.WriteLine("---------------------");
+        }
+
+        private static int amountNonNull (string [] array){
+            int counter = 0;
+            for (int i = 0; i < array.Length; i++){
+                if (array[i] != "null"){
+                    counter++;
+                }
+            }
+            return counter;
+        }
+
+        private static int calcValue (string [] input, char standard, char possible){            
+            int rowLen = input[0].Length;
+
+            for(int i = 0; i < rowLen; i++){
+                if (amountNonNull(input) == 1){
+                    break;
+                }
+
+                int counter = 0;
+                for(int k = 0; k < input.Length; k++){
+                    string word = input[k];
+
+                    if(word == "null"){
+                        continue;
+                    }
+
+                    if (word[i] == '1'){
+                        counter++;
+                    }
+                    if(word[i] == '0'){
+                        counter--;
+                    }
+                }
+                char lookFor = standard;
+                if (counter < 0){
+                    lookFor = possible;
+                }
+                for (int k = 0; k < input.Length; k++){
+                    string word = input[k];
+                    if(word == "null"){
+                        continue;
+                    }
+                    if (word[i] != lookFor){
+                        input[k] = "null";
+                    }
+                }
+            }
+            return binaryToDecimal(findNonNull(input));              
+        }
+
+
+
+        
+
+
     }
 }
