cfe5938d60e032f29425856ca5f44a8970c2c47b
SamuelCr99 <samuel.collier.ryder@gmail.com>
2021-12-08 21:04:52 +0100

    Day 6 stuff

---
+++ Day4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Day4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string [] input = System.IO.File.ReadAllLines("input.txt");                
+            int [] array = buildArray(input[0]);
+            Console.WriteLine(array[1]);
+        }
+
+        private static int[] buildArray(string s){
+            int[] intArr = new int [s.Length];
+            int keepCount = 0;
+            for(int i = 0; i < s.Length; i++){
+                if (s[i] != ','){
+                   intArr[keepCount] = s[i] - '0';
+                   keepCount++; 
+                }
+            }
+            return intArr;
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Day6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+            List<int> list = buildList(lines[0]);
+            fishEvolution(list,80);
+
+        }
+
+        
+
+
+
+        static void fishEvolution(List<int> list, int days){
+            for(int d = 0; d < days; d++){
+                int numberOfLoops = list.Count;
+                for(int fish = 0; fish < numberOfLoops; fish++){
+                    if(list[fish] == 0){
+                        list[fish] = 7;
+                        list.Add(8);
+                    }
+                    list[fish] -= 1;
+                }
+            }
+            Console.WriteLine(list.Count);
+        }
+
+
+        private static List<int> buildList(string s){
+            List<int> newList = new List<int>();
+
+            for(int i = 0; i < s.Length; i++){
+                if(s[i] != ','){
+                    newList.Add(s[i] - '0');
+                }
+            }
+            return newList;
+        }
+
+
+
+    }
+}
